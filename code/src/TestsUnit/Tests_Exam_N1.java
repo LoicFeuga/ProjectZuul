5fbd5d21122eccf37f326d18ffbcffec726602b7
Loïc <loic.feuga@gmail.com>
2014-11-19 13:24:00 +0100

    Tests d'un Exam contenant 20 questions Chaque réponse aux questions est true A la fin du test, la note est donnée

---
+++ code/src/TestsUnit/Tests_Exam_N1.java
@@ -0,0 +1,13 @@
+package TestsUnit;
+
+import model.Exam;
+
+public class Tests_Exam_N1 {
+	
+	public static void main(String[] args) {
+		Exam e = new Exam();
+		e.showQCM();
+		e.takeExam();
+	}
+
+}
