00dc6d472318398b72e069dd1a4c375cba636557
Loïc <loic.feuga@gmail.com>
2014-11-20 09:36:07 +0100

    Class Coffee - Version ok : -Constructeur, getter, setter, equals, clone, toString -Methode drink

--- code/src/model/Coffee.java
+++ code/src/model/Coffee.java
@@ -1,9 +1,70 @@
 package model;
-
+/**
+ * Class Coffee, 
+ * 
+ * @author loic
+ *
+ */
 public class Coffee extends Item {
 	private int energy;
-
-	public void drink() {
-		throw new UnsupportedOperationException();
+	
+	public Coffee(Student student, Room where){
+		super.owner = student;
+		super.position = where;
+		energy = 50;
 	}
+	
+	/**
+	 * Méthode drink, elle octroie le nombre d'energy du coffee 
+	 *   au possesseur du coffee
+	 */
+	public void drink() {
+		if(owner != null){
+			System.out.println("Drinkin");
+			owner.increaseEnergy(energy);
+			owner.deleteItem(this);
+			owner = null;
+			position = null;
+			energy = 0;
+			System.out.println("Coffee drank");
+		}
+	}
+	
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		Coffee coffee = new Coffee(owner, position);
+		return coffee;
+	}
+	
+	@Override
+	public String toString() {
+		String str = "Coffee, if you drink me you will obtain "+energy+" energy\n";
+		return str;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		Coffee item = null;
+		if(obj instanceof Coffee){
+			item = (Coffee) obj;
+		}else{
+			return false;
+		}
+		if(owner.equals(item.owner) && position.equals(item.position) && energy == item.energy){
+			return true;
+		}else{
+			return false;
+		}
+
+	}
+	
+
+	public int getEnergy() {
+		return energy;
+	}
+
+	public void setEnergy(int energy) {
+		this.energy = energy;
+	}
+
 }
