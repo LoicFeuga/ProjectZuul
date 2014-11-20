b482a43c9e56b007d158e67951514a3fd986c6b0
Loïc <loic.feuga@gmail.com>
2014-11-20 09:28:40 +0100

    Class Student - Version ok : -Constructeur, getter,setter, clone, equals, toString, printOwnItem -Méthodes decreasedEnergy, increasedEnergy, -Méthodes getItem, deleteItem -Correction des attributs

--- code/src/model/Student.java
+++ code/src/model/Student.java
@@ -1,14 +1,78 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Student {
 	private int energyLevel;
-	public Item unnamed_Item_;
+	private ArrayList<Item> listItem;
 
-	public void increaseEnergy() {
-		throw new UnsupportedOperationException();
+	public int getEnergyLevel() {
+		return energyLevel;
 	}
 
-	public void decreasedEnergy() {
-		throw new UnsupportedOperationException();
+	public ArrayList<Item> getListItem() {
+		return listItem;
+	}
+
+	public Student(){
+		energyLevel = 100;
+		listItem = new ArrayList<Item>();
+	}
+	
+	public void getItem(Item item){
+		listItem.add(item);
+	}
+	public void deleteItem(Item delete){
+		listItem.remove(delete);
+	}
+	
+	public void increaseEnergy(int number) {
+		energyLevel+=number;
+	}
+	
+	public void printOwnItem(){
+		if(listItem.size() == 0 ) System.out.println("Aucun item possédé");
+		
+		for(int i = 0 ; i < listItem.size(); i++){
+			System.out.println("Item "+(i+1)+" : "+listItem.get(i).toString());
+		}
+	}
+	
+	@Override
+	public String toString() {
+		String str ="";
+		str+="I'm a student, i've "+listItem.size()+" with me, i have "+energyLevel+" left\n";
+		return str;
+	}
+	
+	@Override
+	public boolean equals(Object st) {
+		Student student;
+		
+		if(st instanceof Student){
+			student = (Student) st;
+		}else{
+			return false;
+		}
+		
+		
+		if(energyLevel == student.energyLevel && listItem.equals(student.listItem)){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		Student st = new Student();
+		st.listItem = listItem;
+		return st;
+	}
+	
+	
+	public void decreasedEnergy(int number) {	
+		energyLevel-=number;
 	}
 }
